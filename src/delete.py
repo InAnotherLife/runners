5000673d92927a63f7fd6102c0ba59e53b0d87db
Faye Billye <fbillye@student.21-school.ru>
2024-01-18 01:06:24 +0700

    Add class DeleteResult

---
+++ src/delete.py
@@ -0,0 +1,16 @@
+import os
+
+from settings import Settings
+
+
+class DeleteResult(Settings):
+    def __init__(self):
+        super().__init__()
+
+    def delete_results(self):
+        try:
+            os.remove(self._config.get('Paths', 'competitors_data_file'))
+            os.remove(self._config.get('Paths', 'results_file'))
+            os.remove(self._config.get('Paths', 'output_file'))
+        except FileNotFoundError:
+            print('')
