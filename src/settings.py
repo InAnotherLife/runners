1a8babaedcb7caee0183b03997eaf7849915a00b
Faye Billye <fbillye@student.21-school.ru>
2024-01-18 16:45:52 +0700

    Update comments

--- src/settings.py
+++ src/settings.py
@@ -3,7 +3,11 @@ import configparser
 
 class Settings:
     def __init__(self):
-        # Для работы с файлом конфигурации используется модуль ConfigParser
+        """
+        Родительский класс для работы с файлом конфигурации. Переменная
+        '__settings_file' задает имя файла конфигурации. Для чтения
+        конфигурационного файла используется модуль ConfigParser.
+        """
         __settings_file = 'settings.ini'
         self._config = configparser.ConfigParser()
         self._config.read(__settings_file, encoding='utf-8')
