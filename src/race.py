087163e48a347c6b114c18879dfc3e3d04e06d2e
Faye Billye <fbillye@student.21-school.ru>
2024-01-18 16:35:39 +0700

    Add comments

--- src/race.py
+++ src/race.py
@@ -11,13 +11,25 @@ class Race(Settings):
         self.__results = []
 
     def get_numbers(self):
-        """Чтение файла с номерами спортсменов."""
+        """
+        Чтение файла с номерами спортсменов.
+        arg: None
+        return: Dict
+        """
         with open(self._config.get('Paths', 'competitors_data_file'), 'r',
                   encoding='utf-8') as competitors_data_file:
             competitors_data = json.load(competitors_data_file)
             return competitors_data.keys()
 
     def get_times(self):
+        """
+        Генерация случайного времени забега. Метод получает текущее время и 
+        прибавляет к нему случайное количество минут, секунд и микросекунд.
+        Метод возвращает время старта и время финиша в формате %H:%M:%S,%f в
+        виде строк.
+        arg: None
+        return: Str, str
+        """
         start_time = datetime.datetime.now()
         finish_time = start_time + datetime.timedelta(
             minutes=random.randint(1, 59), seconds=random.randint(0, 59),
@@ -26,6 +38,13 @@ class Race(Settings):
             '%H:%M:%S,%f')
 
     def calc_race(self):
+        """
+        Расчет времени забега для каждого спортсмена. Метод получает номера 
+        спортсменов и для каждого спортсмена генерирует время старта и время
+        финиша забега.
+        arg: None
+        return: None
+        """
         competitors_numbers = self.get_numbers()
         for value in competitors_numbers:
             start_time, finish_time = self.get_times()
@@ -33,17 +52,21 @@ class Race(Settings):
             self.__results += f'{value} финиш {finish_time}\n'
 
     def show_race(self):
+        """
+        Выводит итоговую таблицу с временем забега каждого спортсмена в 
+        консоль.
+        arg: None
+        return: None
+        """
         print(''.join(self.__results))
 
     def save_race(self):
+        """
+        Сохраняет итоговую таблицу с временем забега каждого спортсмена в файл.
+        arg: None
+        return: None
+        """
         with open(self._config.get('Paths', 'results_file'), 'w',
                   encoding='utf-8') as output_file:
             for value in self.__results:
                 output_file.write(str(value))
-
-
-if __name__ == '__main__':
-    race = Race()
-    race.calc_race()
-    race.show_race()
-    race.save_race()
