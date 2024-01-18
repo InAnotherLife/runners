ddcc2893a991c16afa65216bde56119be0fa8c81
Faye Billye <fbillye@student.21-school.ru>
2024-01-18 16:40:59 +0700

    Add comments

--- src/delete.py
+++ src/delete.py
@@ -8,6 +8,12 @@ class DeleteResult(Settings):
         super().__init__()
 
     def delete_results(self):
+        """
+        Удаляет результаты расчета. Метод при возникновении любых исключений
+        ничего не делает.
+        arg: None
+        return: None
+        """
         try:
             os.remove(self._config.get('Paths', 'competitors_data_file'))
             os.remove(self._config.get('Paths', 'results_file'))
