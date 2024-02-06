b56477dbb7e2671287d3d865d98c786876fed886
Faye Billye <fbillye@student.21-school.ru>
2024-02-06 15:21:57 +0700

    Bug fix

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -11,18 +11,20 @@ def test_remove_files():
     config.read(settings_file, encoding='utf-8')
 
     keys = config['Paths']
-    for i, key in enumerate(keys):
+    for i, item in enumerate(keys):
+        key = keys.get(item)
         if key:
-            file = open(keys.get(key), 'w')
+            file = open(key, 'w')
             file.write(f'Test file {i + 1}')
             file.close()
 
     delete = DeleteResult()
     delete.delete_results()
 
-    for key in keys:
+    for item in keys:
+        key = keys.get(item)
         if key:
-            assert not os.path.isfile(keys.get(key))
+            assert not os.path.isfile(key)
 
 
 if __name__ == '__main__':
